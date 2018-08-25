313061ce648010f9406371fca0a9ccf7c20c4c61
Mahvash Fatima <mahvash.sayed@gmail.com>
2018-08-25 17:38:13 +0530

    Add bootstrap cdn links

--- functions.php
+++ functions.php
@@ -122,9 +122,15 @@ add_action( 'widgets_init', 'bace_widgets_init' );
 function bace_scripts() {
 	wp_enqueue_style( 'bace-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'bace-bootstrap-style', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' );
+
 	wp_enqueue_script( 'bace-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'bace-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'bace-bootstrap-script', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array(), '4.1.3', true );
+	wp_enqueue_script( 'bace-jquery-script', 'https://code.jquery.com/jquery-3.3.1.slim.min.js', array(), '3.3.1', true );
+	wp_enqueue_script( 'bace-bootstrap-proper-script', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array(), '1.14.3', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
