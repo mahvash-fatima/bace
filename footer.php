c8ceac100331dda4dd2c6a5f533bd5b3ca6cad2e
Mahvash Fatima <mahvash.sayed@gmail.com>
2018-08-28 12:08:44 +0530

    Register footer widgets, create footer sctructure and add content in footer

--- footer.php
+++ footer.php
@@ -13,9 +13,52 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="container">
-			<p class="site-info">Disclaimer: Sit arcu nec cras elit? Vut sagittis magna nisi vel integer arcu? Dis pulvinar scelerisque pulvinar rhoncus integer, integer in? Ac, cum etiam tortor duis placerat mid nunc cras integer, aliquam porttitor. Dis pulvinar scelerisque pulvinar rhoncus integer, integer in? Ac, cum etiam tortor duis placerat mid nunc cras integer, aliquam porttitor.</p>
-		</div>
+		<div class="bace-footer-content">
+			<div class="container">
+				<div class="row">
+					<div class="bace-site-footer-widgets">
+						<?php
+							if ( is_active_sidebar( 'sidebar-2' ) ){
+								dynamic_sidebar( 'sidebar-2' );
+							}
+						?>
+					</div>
+					<div class="site-info col-md-4">
+						<figure class="bace-site-footer-logo">
+							<?php the_custom_logo(); ?>
+						</figure>
+						<?php
+							printf( esc_html__( '%s', 'bace' ),
+							'<span>Copyrights &copy 2012. All rights reserved.</span>' );
+						?>
+						<br />
+						<?php
+							printf( esc_html__( '%1$s %2$s %3$s %4$s %5$s', 'bace' ),
+								'<a href="https://rtcamp.com/privacy-policy">Terms of Use</a>',
+								'<span class="sep">|</span>',
+								'<a href="https://rtcamp.com/privacy-policy">Privacy Policy</a>',
+								'<span>Designed by',
+								'<a href="https://rtcamp.com">rtCamp</a></span>' );
+						?>
+					</div>
+				</div>
+			</div>
+		</div><!-- .bace-footer-widgets-->
+
+		<div class="bace-site-footer-disclaimer">
+			<div class="container">
+				<p class="bace-site-footer-disclaimer-para">
+					<?php
+					echo sprintf( esc_html__( '%1$s %2$s', 'bace' ),
+						'<strong>Disclaimer: </strong>', 'Sit arcu nec cras elit? Vut sagittis magna nisi vel 
+						integer arcu? Dis pulvinar scelerisque pulvinar rhoncus integer, integer in? Ac, cum etiam 
+						tortor duis placerat mid nunc cras integer, aliquam porttitor. Dis pulvinar scelerisque pulvinar
+						rhoncus integer, integer in? Ac, cum etiam tortor duis placerat mid nunc cras integer, aliquam
+						porttitor.' );
+					?>
+				</p>
+			</div>
+		</div><!-- .bace-footer-site-disclaimer-->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
