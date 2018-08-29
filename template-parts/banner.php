d6576c056bdba054193530e67b6369fd4d9f8bf1
Mahvash Fatima <mahvash.sayed@gmail.com>
2018-08-29 10:57:15 +0530

    Add style in banner content

--- template-parts/banner.php
+++ template-parts/banner.php
@@ -11,9 +11,10 @@
 		</ul>
 	</div>
 	<div class="bace-banner__content col-md-3">
-		<h2>Welcome to rtPanel</h2>
-		<img src="<?php echo get_template_directory_uri(); ?>/img/user.jpg" alt="User">
+		<h2 class="bace-banner__content-title">Welcome to rtPanel</h2>
+		<img src="<?php echo get_template_directory_uri(); ?>/img/user.jpg" alt="User" class="bace-banner__content-image">
 		<p>Vel adipiscing lectus lundium. Dapibus amet turpis adipiscing ridiculus porta, aenean tempor phasellus cras ultrices urna, ut in  turpis auctor, dignissim odio porttitor mus egestas dapibus diam urna phasellus! Amet adipiscing enim, odio odio? Massa eros, ut pulvinar magnis dis, penatibus urna est penatibus turpis egestas non pulvinar, ac lectus sit, habitasse etiam nisi sed habitasse, risus nisi, amet etiam? Pulvinar dis ac vel!</p>
+		<a href="" class="bace-banner__read-more">Read More...</a>
 	</div>
 </div>
 
