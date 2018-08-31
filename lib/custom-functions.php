e15ed29c6018ec36c61e7fe9bd0a8013f4d3c9b8
Mahvash Fatima <mahvash.sayed@gmail.com>
2018-08-31 07:50:02 +0530

    Show most recent post in widget

--- lib/custom-functions.php
+++ lib/custom-functions.php
@@ -18,12 +18,16 @@
 	endif; //bace_our_partners_default_slides
 
 	if( ! function_exists( 'bace_recent_posts' ) ) :
-		function bace_recent_posts() {
-			$args = array( 'posts_per_page' => '3' );
+		function bace_recent_posts()
+		{
+			$first_post_id = false;
+			$args = array( 'posts_per_page' => '1' );
 			$recent_posts = new WP_Query( $args );
-			while( $recent_posts->have_posts() ) {
 
-				$recent_posts->the_post() ; ?>
+			while( $recent_posts->have_posts() )
+			{
+				$recent_posts->the_post() ;
+				$first_post_id = get_the_ID(); ?>
 
 				<article class="bace-recent-post">
 					<?php if ( has_post_thumbnail() ) { ?>
@@ -35,14 +39,31 @@
 						</figure>
 
 					<?php } ?>
+
 					<div class="bace-recent-post__content">
 						<h3 class="bace-recent-post__title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 						<span class="bace-recent-post__date"><?php echo get_the_date(); ?></span>
 					</div>
 				</article>
+			<?php }
 
-				<?php
-			}
+			wp_reset_postdata();
+
+			$args = array( 'posts_per_page' => '3' );
+			$recent_posts = new WP_Query( $args );
+			while( $recent_posts->have_posts() )
+			{
+				$recent_posts->the_post() ;
+
+				if( get_the_ID() === $first_post_id ) {
+					continue;
+				} ?>
+
+				<article class="bace-recent-post">
+					<h3 class="bace-recent-post__title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
+				</article>
+
+			<?php }
 
 			wp_reset_postdata();
 		}
