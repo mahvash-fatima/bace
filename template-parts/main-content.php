329a94d0cd7fa8edf45be73fa6d25f7cdc7c3e89
Mahvash Fatima <mahvash.sayed@gmail.com>
2018-09-02 15:45:49 +0530

    Wrap facebook content in div

--- template-parts/main-content.php
+++ template-parts/main-content.php
@@ -12,11 +12,13 @@
 					<a class="twitter-timeline" data-height="200" href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw">Tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 				</div>
 				<!-- https://developers.facebook.com/docs/plugins/page-plugin/-->
-				<div class="bace-content__facebook fb-page col-md-7" data-href="https://www.facebook.com/babul.ilm.786/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+				<div class="bace-content__facebook col-md-7">
 					<h2 class="bace-title">Follow us on Facebook</h2>
-					<blockquote cite="https://www.facebook.com/babul.ilm.786/" class="fb-xfbml-parse-ignore">
-						<a href="https://www.facebook.com/babul.ilm.786/">Babul ilm</a>
-					</blockquote>
+					<div class="fb-page" data-href="https://www.facebook.com/babul.ilm.786/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+						<blockquote cite="https://www.facebook.com/babul.ilm.786/" class="fb-xfbml-parse-ignore">
+							<a href="https://www.facebook.com/babul.ilm.786/">Babul ilm</a>
+						</blockquote>
+					</div>
 				</div>
 			</div>
 		</div>
